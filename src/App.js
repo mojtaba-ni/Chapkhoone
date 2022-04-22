0c8558bc2d851b500142afd19e2054a3eff8cbac
mojtaba-ni <mojtabanik@yahoo.com>
2022-04-22 15:24:04 +0430

    some initial

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import Off from './layout/Off';
+import { Navbar,Navside,Landing,InfoL,Title} from './AppIMP';
+import { Line,OffPrice,ProductTitle,orderTitle } from './AppIMP';
 import './App.css';
+import Order from './layout/Order';
+import Footer from './layout/Footer';
+import Card from './components/card';
+import Product from './layout/Product';
 
 function App() {
   return (
     <div className="App">
-      hello
+      <Navbar/>
+      <Navside/>
+      <Landing/>
+      <InfoL/>
+      <Title line={Line} Main={OffPrice} />
+      <Off/>
+      <Title line={Line} Main={ProductTitle} />
+      <Product/>
+      <Title line={Line} Main={orderTitle} />
+      <Order/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/AppIMP.js
@@ -0,0 +1,11 @@
+import Line from './assets/offPrice/Line.png'
+import OffPrice from './assets/offPrice/OffPrice.png'
+import ProductTitle from './assets/Product/ProductTitle.png'
+import Navbar from './components/navbar';
+import Navside from './components/navside';
+import Title from './components/title';
+import InfoL from './layout/InfoL';
+import Landing from './layout/Landing';
+import orderTitle from './assets/Order/orderTitle.png'
+
+export {Line,OffPrice,ProductTitle,orderTitle,Navbar,Navside,Title,InfoL,Landing}
