845a8ff8e45171ca28f15b0c6089c9927d1145ae
mojtaba-ni <mojtabanik@yahoo.com>
2022-04-23 22:05:27 +0430

    add some fatures on Off layout

--- src/layout/Off.jsx
+++ src/layout/Off.jsx
@@ -1,11 +1,16 @@
 
-import React from 'react'
+import React,{useState} from 'react'
 import OffLand from '../assets/Landing/OffLand.png'
 import Card from '../components/card'
 import { Subject } from '../components/card'
+import product1 from '../assets/Product/product1.png'
+import product2 from '../assets/Product/product2.png'
+import product3 from '../assets/Product/product3.png'
 import styles from '../styles/layout/off.module.scss'
 
 export default () => {
+  
+  const width = window.outerWidth;
   return (
     <div className={styles.offContainer}>
         <div className={styles.offBox}>
@@ -19,20 +24,46 @@ export default () => {
                     <Subject title={"کارت ویزیت"}/>  
                   </div>
                   <div className={styles.subjectLi}>
-                    <Subject title={"کارت ویزیت"}/>  
+                    <Subject title={"تقویم رومیزی"}/>  
                   </div>
                 </div>
                 <div className={styles.offBody}>
                   <div className={styles.bodyLi}>
-                    <Card/>
+                    <Card img={product1} title={"لمینت براق"} />
                   </div>
                   <div className={styles.bodyLi}>
-                    <Card/>
+                    <Card img={product2} title={"مخملی طلاکوب"} />
                   </div>
                   <div className={styles.bodyLi}>
-                    <Card/>
+                    <Card img={product3} title={"لمینت مات"} />
                   </div>
                 </div>
+            </div>
+        </div>
+        <div className={styles.offMobile}>
+            <div className={styles.offMobileWrapper}>
+              <div className={styles.offHeader}>
+                <div className={styles.headerLi}>
+                  <Subject className={`width:${width}`} title={'پاکت نانه'} />
+                </div>
+                <div className={styles.headerLi}>
+                  <Subject className={`width:${width}`} title={'پاکت نانه'}/>
+                </div>
+                <div className={styles.headerLi}>
+                  <Subject className={`width:${width}`} title={'پاکت نانه'}/>
+                </div>
+              </div>
+              <div className={styles.offBody}>
+                  <div className={`width:${width}`}>
+                    <Card img={product1} title={"لمینت براق"} />
+                  </div>
+                  <div className={`width:${width}`}>
+                    <Card img={product2} title={"مخملی طلاکوب"} />
+                  </div>
+                  <div className={`width:${width}`}>
+                    <Card img={product3} title={"لمینت مات"} />
+                  </div>
+              </div>
             </div>
         </div>
     </div>
