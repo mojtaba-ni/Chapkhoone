2e90e2579c3eefb5f046ff1c1c7a7413a75bcc3b
mojtaba-ni <mojtabanik@yahoo.com>
2022-04-23 22:05:43 +0430

    create Sidebar Info

---
+++ src/utils/sidebarInfo.js
@@ -0,0 +1,30 @@
+import MailIcon from "@material-ui/icons/Mail";
+import PhoneEnabledRoundedIcon from '@material-ui/icons/PhoneEnabledRounded';
+
+export const sidebar = [
+    {   
+        id:0,
+        name:'صفحه اصلی',
+        icon: <MailIcon />,
+    },
+    {   
+        id:1,
+        name:'محصولات',
+        icon: <MailIcon />,
+    },
+    {   
+        id:2,
+        name:'لیست قیمت',
+        icon: <MailIcon />,
+    },
+    {   
+        id:3,
+        name:'قالب ها',
+        icon: <MailIcon />,
+    },
+    {   
+        id:4,
+        name:'تماس با ما',
+        icon: <PhoneEnabledRoundedIcon />,
+    },
+]
