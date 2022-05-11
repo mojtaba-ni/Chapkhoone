f0ec3c32e645549eb736e0b3902970b1780b08fa
mojtaba-ni <mojtabanik@yahoo.com>
2022-05-11 15:23:10 +0430

    add Animation utils

---
+++ src/utils/Animation.js
@@ -0,0 +1,107 @@
+export const NavbarAnim = {
+    hidden:{
+        y:-100,
+    },
+    visible:{
+        y:0,
+        transition:{
+            duration:.8,
+            delay:.6
+        }
+    }
+}
+export const NavsideAnim = {
+    hidden:{
+        opacity:0,
+        y:-150,
+    },
+    visible:{
+        y:0,
+        opacity:1,
+        transition:{
+            duration:1,
+            delay:1
+        }
+    }
+}
+export const landTitle = {
+    hidden:{
+        x:-740,
+    },
+    visible:{
+        x:0,
+        transition:{
+            duration:1,
+            delay:1.9
+        }
+    }
+}
+export const landImg = {
+    hidden:{
+        opacity:0
+    },
+    visible:{
+        opacity:1,
+        transition:{
+            duration:1,
+            delay:.8,
+        }
+    }
+}
+export const InfoAnim = {
+    hidden:{
+        y:-200,
+        opacity:0
+    },
+    visible:{
+        y:0,
+        opacity:1,
+        transition:{
+            duration:1.5,
+            delay:2.4
+        }
+    }
+}
+export const landInfo = {
+    hidden:{
+        x:-1200,
+        opacity:0
+    },
+    visible:{
+        x:0,
+        opacity:1,
+        transition:{
+            duration:1.2,
+            delay:1
+        }
+    }
+}
+export const UpAnim = {
+    hidden:{
+        opacity:0,
+        y:-150,
+    },
+    visible:{
+        y:0,
+        opacity:1,
+        transition:{
+            duration:.7,
+            delay:.2
+        }
+    }
+}
+export const RightAnim = {
+    hidden:{
+      
+        opacity:0,
+    },
+    visible:{
+  
+        opacity:1,
+   
+        transition:{
+            duration:1.1,
+            delay:.5,
+        }
+    }
+}
